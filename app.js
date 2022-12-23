ea3ce686e8abe5fa22a0f088bcb1e84b886a9a49
Béba Godfried <66435433+bebagodfried@users.noreply.github.com>
2022-12-23 05:26:19 +0000

    Release 1.0.0

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-import * as d from './typeacript/_mod/date'
-console.log(d.date('Y'))
+// package informations
+let design = selectId('fwk')
+    design.innerHTML = `elvys`
+
+// navigation
+const nav = select('nav a', true)
+      nav.forEach(item => {
+        if (item.href == $_('url')) {
+             item.style.color = 'red';
+        }
+      });
+
+echo(date('Y'), '#copyright')
